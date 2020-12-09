1e213f1cc5e70d6dedffcd60426d993d223f4866
makutak <takkouno@gmail.com>
2020-12-10 05:07:10 +0900

    ひとまず外観

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use iced::{executor, Application, Command, Element, Settings, Text};
+use iced::{
+    button, executor, Align, Application, Button, Column, Command, Element, Font,
+    HorizontalAlignment, Length, Row, Settings, Subscription, Text,
+};
 
-struct GUI;
+const FONT: Font = Font::External {
+    name: "PixelMplus12-Regular",
+    bytes: include_bytes!("../rsc/PixelMplus12-Regular.ttf"),
+};
+
+struct GUI {
+    start_stop_button_state: button::State,
+    reset_button_state: button::State,
+}
 
 impl Application for GUI {
     type Executor = executor::Null;
@@ -8,7 +19,13 @@ impl Application for GUI {
     type Flags = ();
 
     fn new(_flags: ()) -> (GUI, Command<Self::Message>) {
-        (GUI, Command::none())
+        (
+            GUI {
+                start_stop_button_state: button::State::new(),
+                reset_button_state: button::State::new(),
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
@@ -20,7 +37,39 @@ impl Application for GUI {
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        Text::new("Pomodoro Timer").into()
+        // init widgets
+        let tick_text = Text::new("25:00.00").font(FONT).size(60);
+        let start_stop_button = Button::new(
+            &mut self.start_stop_button_state,
+            Text::new("Start")
+                .horizontal_alignment(HorizontalAlignment::Center)
+                .font(FONT),
+        )
+        .min_width(80);
+
+        let reset_button = Button::new(
+            &mut self.reset_button_state,
+            Text::new("Start")
+                .horizontal_alignment(HorizontalAlignment::Center)
+                .font(FONT),
+        )
+        .min_width(80);
+
+        // prepare column
+        Column::new()
+            .push(tick_text)
+            .push(
+                Row::new()
+                    .push(start_stop_button)
+                    .push(reset_button)
+                    .spacing(10),
+            )
+            .spacing(10)
+            .spacing(10)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .align_items(Align::Center)
+            .into()
     }
 }
 
